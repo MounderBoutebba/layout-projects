1de2b8209466133ddc9ff5a273c7e2a1f40e3ec7
MounderBoutebba <mounderboutebba@outlook.fr>
2019-10-21 11:32:20 +0100

    finished carousel section

---
+++ blog website example/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(
+    function () {
+        $nav = $('.nav');
+        $toggleCollapse = $('.toggle-collapse');
+
+        $toggleCollapse.click(function () {
+            $nav.toggleClass('collapse');
+        })
+
+        //owl
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            dots: false,
+            nav: true,
+            navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        });
+    }
+
+
+
+)
