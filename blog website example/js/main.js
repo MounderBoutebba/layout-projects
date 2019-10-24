ce6b4ade20a185a8585ee5c651f19f39fc03dd1c
MounderBoutebba <mounderboutebba@outlook.fr>
2019-10-24 11:39:29 +0100

    responsiveness

--- blog website example/js/main.js
+++ blog website example/js/main.js
@@ -1,3 +1,10 @@
+const responsive = {
+    0: { items: 1 },
+    320: { items: 1 },
+    560: { items: 2 },
+    960: { items: 3 }
+}
+
 $(document).ready(
     function () {
         $nav = $('.nav');
@@ -14,10 +21,16 @@ $(document).ready(
             autoplayTimeout: 3000,
             dots: false,
             nav: true,
-            navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+            navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+            responsive: responsive
+
         });
-    }
 
+        //scroll top
+        $('.move-up span').click(function () {
+            $('html,body').animate({
+                scrollTop: 0
+            }, 1000);
+        })
 
-
-)
+    });
